687c4dc8c8b0c65cefcefe7e6421844d605279ff
Kelley Parker <kelley.parker07@gmail.com>
2023-11-01 17:13:26 -0500

    Update generate_container.ps1

--- multistreaming/generate_container.ps1
+++ multistreaming/generate_container.ps1
@@ -1,2 +1,5 @@
-docker build -t rtmp-multistream .
-docker run -p 1935:1935 rtmp-multistream
+# Step 1: Pull the Docker image
+docker pull tiangolo/nginx-rtmp
+
+# Step 2: Run the Docker container
+docker run -d -p 1935:1935 --name rtmp-multistreaming tiangolo/nginx-rtmp
